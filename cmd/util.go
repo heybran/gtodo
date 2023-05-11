abde2f6ce9ce3bfda43a10d852ebdf42080c5341
brandon <gwonzhang@gmail.com>
2023-05-11 22:43:26 +0800

    add util function from previous main.go

--- cmd/util.go
+++ cmd/util.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"os"
+	"github.com/heybran/gtodo/todo"
 	"log"
 	"path/filepath"
+	"strings"
+	"bufio"
+	"fmt"
 )
 
 // getJsonFile will grab the .todos.json file located at user home directory
@@ -14,4 +18,39 @@ func GetJsonFile() string {
 	}
 
 	return filepath.Join(homeDir, ".todos.json")
+}
+
+// getUserApproval will get the user's approval when creating an empty json file
+func GetUserApproval() (bool) {
+	confirmMessage := "Need to create an empty \".todos.json\" file in your home directory to store your todo items, continue? (y/n): "
+
+	r := bufio.NewReader(os.Stdin)
+	var s string
+
+	fmt.Print(confirmMessage)
+	s, _ = r.ReadString('\n')
+	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
+
+	for {
+		if s == "y" || s == "yes" {
+			return true
+		}
+		if s == "n" || s == "no" {
+			return false
+		}
+	}
+}
+
+func RemindInit(todos *todo.Todos) {
+	// check if .todos.json already exists in user home directory
+	_, err := os.Stat(GetJsonFile())
+	if err != nil {
+		fmt.Println("Please run \"init\" subcommand to create an JSON file to store your todo items.")
+		os.Exit(1)
+	} else {
+		if err := todos.Load(GetJsonFile()); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
